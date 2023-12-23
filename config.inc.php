05a62191c7b4c2a7b5c98d17a6849dbb14c5e853
danj0709 <139046499+danj0709@users.noreply.github.com>
2023-12-24 01:41:27 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -27,12 +27,12 @@ Typecho_Common::init();
 /** 定义数据库参数 */
 $db = new Typecho_Db('Pdo_Mysql', 'typecho_');
 $db->addServer(array (
-  'host' => 'databasehost',
-  'user' => 'databaseuser',
-  'password' => 'password',
+  'host' => 'roundhouse.proxy.rlwy.net',
+  'user' => 'root',
+  'password' => 'FBhAbdeDC-B2-fh23g4fbBc532C2hdEf',
   'charset' => 'utf8mb4',
-  'port' => 'port',
-  'database' => 'databasename',
+  'port' => '54213',
+  'database' => 'railway',
   'engine' => 'MyISAM',
 ), Typecho_Db::READ | Typecho_Db::WRITE);
 Typecho_Db::set($db);
